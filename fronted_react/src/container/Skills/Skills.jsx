78d7d5b635810f7b087a3a3346a1db1224ffc41e
guifu zhang <gzha0010@uni.sydney.edu.au>
2024-05-13 16:40:11 +1000

    update sanity config

--- fronted_react/src/container/Skills/Skills.jsx
+++ fronted_react/src/container/Skills/Skills.jsx
@@ -38,7 +38,7 @@ const Skills = () => {
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className="app__skills-item app__flex"
-              key={skill.name}
+              key={skill.index}
             >
               <div
                 className="app__flex"
